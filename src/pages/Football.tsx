0cb2705067fb7047819131f394ecc313a02d5c82
Nanda <58926775+gumelartresnadwinanda@users.noreply.github.com>
2025-03-17 12:34:26 +0700

    fix: Adjust padding in FootballPage component for improved layout

--- src/pages/Football.tsx
+++ src/pages/Football.tsx
@@ -6,7 +6,7 @@ const PlaylistPage = () => {
 
   return (
     <Container >
-      <div className="w-full h-86 md:h-1116 lg:h-160 pb-20">
+      <div className="w-full h-86 md:h-1116 lg:h-160 py-20">
         <iframe
           style={{ border: 0, height: "100%", width: "100%" }}
           src={`https://givemereddit.eu/soccer/${team || 'liverpool'}.html`}
